97dc07f8b2534d6b453f8f084cc1460bc8986d2c
Julian <jaquimbayoc7@gmail.com>
2024-04-08 20:52:37 -0500

    rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,16 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { DirectivaComponent } from './directiva/directiva.component';
 import { ClientesComponent } from './clientes/clientes.component';
+import { Router, RouterModule, Routes } from '@angular/router';
+import { ClienteService } from './clientes/cliente.service';
+
+
+//configuración de enrutamiento
+const routes:Routes = [
+  {path:'', redirectTo:'/clientes', pathMatch:'full'},
+  {path:'directiva', component:DirectivaComponent},
+  {path:'clientes', component:ClientesComponent}
+]
 
 
 @NgModule({
@@ -17,9 +27,10 @@ import { ClientesComponent } from './clientes/clientes.component';
     ClientesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [ClienteService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
